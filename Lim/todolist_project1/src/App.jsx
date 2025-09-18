2e459f78a1b05f84c527ebdace67ad6bb3ecebb1
in7570 <i7570@naver.com>
2025-09-18 13:24:54 +0900

    Feat: DnD, Toast 라이브러리 추가 등 1. @hello-pangea/dnd 라이브러리로 리스트 드래그 앤 드롭 구현 2. Toast 라이브러리로 input값 미입력 후 onAdd()호출 시 토스트메세지 구현 3. Fix: inputbox 중복 이벤트 오류 4. Design: Card,Input,App.module.css 수정

--- Lim/todolist_project1/src/App.jsx
+++ Lim/todolist_project1/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Toaster } from 'react-hot-toast';
 import TitleBox from './components/TitleBox';
 import InputBox from './components/InputBox';
 import Cards from './components/Cards';
@@ -7,23 +8,55 @@ import './App.css';
 
 function App() {
   const [todos, setTodos] = useState([]);
-  // let todos=[];
-  // onAdd prop을 통해 InputBox 컴포넌트에서 새로운 할 일이 추가될 때마다 todos 상태를 업데이트합니다.
-  // onAdd는 들어온 값을 setTodos를 통해 todos배열에 추가함
+
+  const onAdd = (text) => {
+    setTodos((prev) => [...prev, { id: Date.now(), text }]);
+    // ...prev : 기존 배열 복사
+    // 예를 들어, todos가 [{id:1, text:'task1'}] 였다면
+    // onAdd('task2') 호출 후에는 [{id: 1, text:'task1'}, {id: 2, text:'task2'}] 가 됨
+  };
+
+  const onDelete = (todoId) => {
+    // _: 언더스코어는 사용하지 않는 매개변수를 나타낼 때 관례적으로 사용
+    //
+    setTodos((prev) => prev.filter((e) => e.id !== todoId));
+    // 지금 보고있는 요소의 id가 삭제할 요소의 id와 다른가요?
+    // 그렇다면 값을 반환합시다(배열로)
+    // 아니라면 필터링합시다
+    // 원래는 index로 했는데 버그를 유발한대서 id로 바꾸었따
+
+    // 현재요소의 인덱스 i 가 삭제할 요소의 index와 같지 않으면 남김
+    // filter는 true인 요소만 남기고 false인 요소는 제거
+    // 즉 현재요소와 삭제할 요소가 같으면 false가 되서 제거됨
+  };
+
+  const onDragEnd = (result) => {
+    const { source, destination } = result;
+    // onDragEnd인자 result는 { source, destination, draggableId, ...} 구조임
+    // 드래그가 유효한지 확인
+    if (!destination) return;
+
+    // 아이템이 같은 위치로 드래그된 경우
+    if (source.index === destination.index) return;
+    // 아이템 재배치
+    const updatedTodos = Array.from(todos);
+    // Array.from : 유사 배열 객체나 반복 가능한 객체를 얕게 복사하여 새로운 배열 객체를 생성
+    const [movedItem] = updatedTodos.splice(source.index, 1);
+    // source.index 위치에서 1개의 요소를 제거하고, 제거된 요소를 movedItem에 저장
+    // => 결국 source index 위치의 요소를 배열에 넣은거임 원본 배열에서는 삭제!!
+    // 근데 사실상 todos 복제본이라서 todos에는 영향 x
+
+    updatedTodos.splice(destination.index, 0, movedItem);
+    // .splice(삽입할 위치, 삭제할 요소의 개수, 삽입할 요소)
+    // 목적지에 movedItem 삽입
+    setTodos(updatedTodos);
+  };
   return (
     <div className='root'>
+      <Toaster />
       <TitleBox />
-      <InputBox onAdd={(value) => setTodos((prev) => [...prev, value])} />
-      <Cards
-        items={todos}
-        onDelete={
-          (index) => setTodos((prev) => prev.filter((_, i) => i !== index))
-          // filter를 사용하여 할 일을 삭제한다.
-          // filter는 조건에 맞는 요소들로만 새로운 배열을 만듦
-          // index는 삭제할 요소의 인덱스, i는 현재 요소의 인덱스
-          // i가 index와 같지 않은 요소들만 남겨서 새로운 배열을 만듦
-        }
-      />
+      <InputBox onAdd={onAdd} />
+      <Cards items={todos} onDelete={onDelete} onDragEnd={onDragEnd} />
     </div>
   );
 }
--- Lim/todolist_project1/src/components/InputBox.jsx
+++ Lim/todolist_project1/src/components/InputBox.jsx
@@ -1,16 +1,24 @@
 import { useState } from 'react';
+import { toast } from 'react-hot-toast';
 import styles from './css/InputBox.module.css';
 
 function InputBox({ onAdd }) {
   const [inputValue, setInputValue] = useState('');
 
   const insertList = () => {
+    if (inputValue === '') toast.error('입력해주세요~!');
     if (inputValue.trim()) {
       onAdd(inputValue);
       setInputValue('');
     }
   };
-
+  const onKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      e.preventDefault(); // 기본 동작 방지 (줄바꿈 방지)
+      if (e.nativeEvent.isComposing) return; // 한글 입력 중일 때는 무시
+      insertList();
+    }
+  };
   return (
     <div className={styles.container}>
       <input
@@ -19,13 +27,9 @@ function InputBox({ onAdd }) {
         placeholder='할 일을 입력하세요'
         value={inputValue}
         onChange={(e) => setInputValue(e.target.value)}
-        onKeyDown={(e) => {
-          if (e.key === 'Enter') {
-            insertList();
-          }
-        }}
+        onKeyDown={onKeyDown}
       />
-      <button onClick={insertList}>추가</button>
+      <button onClick={insertList}>➕</button>
     </div>
   );
 }

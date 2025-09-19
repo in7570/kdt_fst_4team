ba4a07ab5fdf8a9f715597c50727886e6fa80c25
KimHyeonSu <hirobin2001@gmail.com>
2025-09-19 14:20:04 +0900

    Feat: 추가 password.html, password.js
    비밀번호 변경 페이지 제작, 로그인 페이지랑 회원가입 페이지랑 서로 연결

---
+++ KHS/password.js
@@ -0,0 +1,30 @@
+document.querySelectorAll('.watch-pwd-open').forEach(openIcon => {
+    openIcon.addEventListener('click', (event) => {
+        const container = event.target.parentElement;
+        const input = container.querySelector('input');
+        const closeIcon = container.querySelector('.watch-pwd-close');
+
+        if(input && closeIcon) {
+            input.type = 'text';
+            event.target.classList.add('hidden');
+            closeIcon.classList.remove('hidden');
+        }
+    });
+});
+
+document.querySelectorAll('.watch-pwd-close').forEach(closeIcon => {
+    closeIcon.addEventListener('click', (event) => {
+        const container = event.target.parentElement;
+        const input = container.querySelector('input');
+        const openIcon = container.querySelector('.watch-pwd-open');
+
+        if(input && openIcon) {
+            input.type = 'password';
+            event.target.classList.add('hidden');
+            openIcon.classList.remove('hidden');
+        }
+    });
+});
+
+// 변경된 비밀번호 db에 저장
+// 다시 로그인 페이지로 돌아가서 로그인하기

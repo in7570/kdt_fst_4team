bf0a0307d7f1d5c965fb1f225fbf8a60b90b1733
KimHyeonSu <hirobin2001@gmail.com>
2025-09-28 22:11:32 +0900

    Feat: password post 기능 추가(임시)
    임시로 기능 만들어 놓고 나중에 수정

--- KHS/password.js
+++ KHS/password.js
@@ -26,7 +26,63 @@ document.querySelectorAll('.watch-pwd-close').forEach(closeIcon => {
     });
 });
 
+const submitBtn = document.querySelector('.submit-btn');
+if (submitBtn) {
+    submitBtn.addEventListener('click', (event) => {
+        event.preventDefault();
 
+        const newPasswordInput = document.getElementById('new-password');
+        const confirmNewPasswordInput = document.getElementById('confirm-new-password');
+
+        if (newPasswordInput.value.trim() === '') {
+            alert('새 비밀번호를 입력해주세요.');
+            newPasswordInput.focus();
+            return;
+        }
+        if (newPasswordInput.value.length < 3 || newPasswordInput.value.length > 10) {
+            alert(`새 비밀번호는 ${3}자 이상 ${10}자 이하로 입력해주세요.`);
+            newPasswordInput.focus();
+            return;
+        }
+
+        if (confirmNewPasswordInput.value.trim() === '') {
+            alert('새 비밀번호 확인을 입력해주세요.');
+            passwordInput.focus();
+            return;
+        }
+        if (newPasswordInput.value !== confirmNewPasswordInput.value) {
+            alert('새 비밀번호가 일치하지 않습니다. 다시 입력해 주세요.');
+            confirmPasswordInput.focus();
+            return;
+        }
+
+        const newPwdData = {
+            id: idInput.value,
+            pwd: passwordInput.value,
+        };
+
+        fetch('/password', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(newPwdData)
+        })
+        .then(response => response.json())
+        .then(data => {
+            if(data.message === "비밀번호 변경 성공!") {
+                alert('비밀번호 변경 성공!');
+                window.location.href = '/login';
+            } else {
+                alert(data.message);
+            }
+        })
+        .catch(error => {
+            console.log('Error:', error);
+            alert('비밀번호 변경 중 오류가 발생했습니다.');
+        });
+    });
+}
 
 // 변경된 비밀번호 db에 저장
 // 다시 로그인 페이지로 돌아가서 로그인하기
